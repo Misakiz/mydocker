9d8188f1a9b3996d1f55b015efa2bf826de30d7f
邹泉安 <anquan.zou@going-link.com>
2023-02-08 20:46:57 +0800

    [ADD]自动添加隔离hostname,创建一个子进程修改，中间套一层

--- run/run.go
+++ run/run.go
@@ -9,23 +9,26 @@ import (
 
 func MyDockerRun() {
 	fmt.Println(os.Args)
-	fmt.Println(len(os.Args))
+	fmt.Printf("Process -> %v [%d]\n", os.Args, os.Getpid())
 	if len(os.Args) <= 1 {
 		panic("!")
 	}
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("have no define")
 	}
 }
 
 func run() {
-	cmd := exec.Command(os.Args[2])
+	//开了一个子进程
+	cmd := exec.Command(os.Args[0], append([]string{"child"}, os.Args[2])...)
 	//创建SysProcAttr对象，为生产的进程设置uts隔离
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	//将os  /dev/stdin的标准输入输出 给cmd
 	cmd.Stdin = os.Stdin
@@ -35,4 +38,15 @@ func run() {
 		panic(err)
 	}
 
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2])
+	syscall.Sethostname([]byte("container"))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
 }
